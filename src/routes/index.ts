b545240a7e7eb6fab41e422ef074e9a97630eae3
chanyang217 <chanyang721@gmail.com>
2021-10-01 22:20:44 +0900

    DELETE 주석

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -12,8 +12,3 @@ export default (app: Router) => {
     app.use("/api", studentRouter);
 
 }
-
-
-// export { default as instructorRouter } from "./instructor";
-// export { default as lectureRouter } from "./lecture";
-// export { default as studentRouter } from "./student";
